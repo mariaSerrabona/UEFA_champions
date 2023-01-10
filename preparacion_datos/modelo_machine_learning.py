ae8a45671f8abf6dc9cdad0a27e59fcfc5f7c049
mariaSerrabona <83339874+mariaSerrabona@users.noreply.github.com>
2023-01-10 22:36:56 +0100

    resulmen del modelo

---
+++ preparacion_datos/modelo_machine_learning.py
@@ -0,0 +1,3 @@
+# En este fichero se quiere crear un modelo de machine learning en el que se entrene a la red con los datos que tenemos
+# y las probabilidades que se van a calcular. De esta forma, al introducir una nueva apuesta, nuestro modelo será lo
+#suficientemnete preciso como para que nos estime una probabilidad de acierto.
